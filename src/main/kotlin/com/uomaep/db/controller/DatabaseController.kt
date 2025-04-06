0c787ea9123a358abcdd757c74470977bfa147d4
Daewon Lim <uomaep@gmail.com>
2025-04-07 01:23:33 +0900

    fix: 데이터베이스 이름 유효성 검사 로직

--- src/main/kotlin/com/uomaep/db/controller/DatabaseController.kt
+++ src/main/kotlin/com/uomaep/db/controller/DatabaseController.kt
@@ -18,11 +18,6 @@ class DatabaseController(
 ) {
     @Value("\${admin.account}") private lateinit var adminAccount: String
 
-    fun String.hasSpecialCharacter(): Boolean {
-        val regex = Regex("[^a-zA-Z0-9]")
-        return regex.containsMatchIn(this)
-    }
-
     @PostMapping("/create")
     @ResponseBody
     fun createDatabase(@RequestParam databaseName: String, session: HttpSession): Map<String, Any> {
@@ -32,7 +27,7 @@ class DatabaseController(
             return mapOf(Pair("result", false), Pair("msg", "관리자에 의해 데이터베이스 생성이 차단되었습니다."))
         }
 
-        if(databaseName.hasSpecialCharacter())
+        if(!isValidDatabaseName(databaseName))
             return mapOf(Pair("result", false), Pair("msg", "잘못된 데이터베이스 이름입니다."))
 
         if(userService.isExistDatabase(databaseName) || databaseService.isExistDatabase(databaseName))
@@ -49,5 +44,9 @@ class DatabaseController(
     fun dropDatabase(@RequestBody database: UserDatabaseDTO, session: HttpSession): Boolean {
         var user = session.getAttribute("user") as UserDTO
         return !databaseService.drop(database).isFailure
+    }
+
+    fun isValidDatabaseName(account: String): Boolean {
+        return account.matches(Regex("^[^\\s]{4,20}$"))
     }
 }
